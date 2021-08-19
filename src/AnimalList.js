c17c2669c0113b96a4fa33082cd7f43775d802f6
Alice-Retz <alicegretz@gmail.com>
2021-08-18 19:56:53 -0500

    finally fixed join issue, should be working as intended

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getAnimals } from './fetch-utils';
 import { Link } from 'react-router-dom';
+import './Styles/animalList.css';
 
 class AnimalList extends Component {
     state = { animals: [] };
@@ -12,11 +13,14 @@ class AnimalList extends Component {
         return ( 
             <section className="animal-list">
                 {this.state.animals.map((a) => (
+                  <div className="cartoon-card" key={a.id}> 
                     <h2>
                         <Link to={`/animals/${a.id}`}>
                         {a.name}</Link>
                     </h2>
-                        
+                    <img src={a.img} alt="the official in-game sprite for this animal" />
+                    <p>Produces: {a.produces}</p>
+                  </div>      
                 ))}
             </section>
          );
